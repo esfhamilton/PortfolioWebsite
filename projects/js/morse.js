0896f115f03ff3ac106e049c784ebfe8bb5dbed9
esfhamilton <esfhamilton@gmail.com>
2020-04-06 22:28:21 +0100

    Improvements to Morse Simulator

--- projects/js/morse.js
+++ projects/js/morse.js
@@ -8,32 +8,39 @@ window.onload=function() {
     let btnMorse = document.getElementById("btn-morse");
     let incBool =false;
     
-    let temp = document.getElementById("txtBox");
-        temp.innerHTML = txt;
+    
     
     btnDownInterval = setInterval(btnDownFunc,1000/300);
+    clearInterval(btnDownInterval);
     
-    btnMorse.onclick = function() {
-        temp.innerHTML = txt;
-    }
+    btnUpInterval = setInterval(btnUpFunc,1000/300);
+    clearInterval(btnUpInterval);
+    
+/*    btnMorse.onclick = function() {
+        if(timeUp>80){
+            txt += morseToText(letter);
+            letter="";
+            temp.innerHTML = txt;
+        }
+    }*/
     
     btnMorse.onmousedown = function() {
-        timeDown = 0;
         beep.play();
+        timeDown = 0;
+        clearInterval(btnUpInterval);
         btnDownInterval = setInterval(btnDownFunc,1000/300);
+        
+
     };
 
     btnMorse.onmouseup = function() {
         beep.pause();
         beep.currentTime = 0;
+        timeUp = 0;
         clearInterval(btnDownInterval);
+        btnUpInterval = setInterval(btnUpFunc,1000/300);
         
-        if(timeDown>100){
-            txt+="-";
-        }
-        else{
-            txt+=".";
-        }
+        letter += (timeDown>56 ? "-":"•");
         
         console.log("STOPPED at: "+timeDown+txt);
     };      
@@ -41,10 +48,72 @@ window.onload=function() {
 }
 
 timeDown = 0;
+timeUp = 0;
 txt = "";
+letter = "";
+let txtString = document.getElementById("txt");
+txtString.innerHTML = txt;
+let morseString = document.getElementById("morse");
+morseString.innerHTML = letter;
 
 function btnDownFunc() {
     timeDown++;
+}
+
+function btnUpFunc() {
+    timeUp++;
+    if(timeUp>80){
+        txt += morseToText(letter);
+        letter="";
+        txtString.innerHTML = txt;
+    }
+}
+
+/*
+    Mappings for dots and dashes to characters based on International Morse Code
+*/
+function morseToText(letter) {
+    mapping = {"•-":"A",
+               "-•••":"B",
+               "-•-•":"C",
+               "-••":"D",
+               "•":"E",
+               "••-•":"F",
+               "--•":"G",
+               "••••":"H",
+               "••":"I",
+               "•---":"J",
+               "-•-":"K",
+               "•-••":"L",
+               "--":"M",
+               "-•":"N",
+               "---":"O",
+               "•--•":"P",
+               "--•-":"Q",
+               "•-•":"R",
+               "•••":"S",
+               "-":"T",
+               "••-":"U",
+               "•••-":"V",
+               "•--":"W",
+               "-••-":"X",
+               "-•--":"Y",
+               "--••":"Z",
+               "•----":"1",
+               "••---":"2",
+               "•••--":"3",
+               "••••-":"4",
+               "•••••":"5",
+               "-••••":"6",
+               "--•••":"7",
+               "---••":"8",
+               "----•":"9",
+               "-----":"0"};
+    if(mapping[letter]==undefined){
+        return(" ")
+    }else{
+        return(mapping[letter]);
+    }
 }
 
 // Called on keydown event 
